52169f63e2a4a23539772990aaa5f043bf8f37a3
MishaHnatyshyn <gnatyshynmisha@gmail.com>
2019-04-14 12:47:40 +0300

    Pagination

--- src/client/components/LotList/Lots.js
+++ src/client/components/LotList/Lots.js
@@ -1,32 +1,8 @@
 import React from 'react';
 
 import LotCard from './LotCard';
-
-const photos = [
-  'https://i2.rozetka.ua/goods/1784911/apple_airpods_images_1784911319.jpg',
-  'https://i1.rozetka.ua/goods/1127297/hyperx_cloud_core_images_1127297246.jpg',
-  'https://i1.rozetka.ua/goods/1882711/xiaomi_hsej03jy_black_images_1882711880.jpg',
-  'https://i2.rozetka.ua/goods/434611/kingston_hyperx_cloud_ii_gun_metal_images_434611672.jpg',
-  'https://i1.rozetka.ua/goods/1741362/hyperx_hx_hscs_bk_ee_images_1741362158.jpg',
-  'https://i2.rozetka.ua/goods/10462238/panasonic_rp_hje125e_k_images_10462238132.jpg',
-  'https://i2.rozetka.ua/goods/2148230/hyperx_hx_hsca_rdee_images_2148230695.jpg',
-  'https://i1.rozetka.ua/goods/11167527/samsung_sm_r170nzkasek_images_11167527579.jpg',
-  'https://i2.rozetka.ua/goods/2067969/kingston_hx_hscl_sr_na_images_2067969064.jpg',
-];
-
-const LotsData = photos.map((photo, index) => ({
-  name: `Lot ${index}`,
-  description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam aliquid at autem consequatur dignissimos, dolore eveniet in inventore ipsam molestias nam non odit porro quae quasi quos, repellat ullam!',
-  startPrice: 100 * (index + 1),
-  currPrice: 200 * (index + 1),
-  photos: [photo],
-  payment: ['Cash', 'Visa', 'Mastercard', 'PayPal', 'Other'],
-  delivery: ['Post office', 'Personal meeting', 'Courier', 'Other'],
-  timestamp: new Date(Date.parse(new Date()) - 86400 * index),
-}));
-
-const pagesCount = 5
-
+import Pagination from "./Pagination";
+import LotsData from "./StaticData";
 
 // #TODO filter bugs
 
@@ -40,22 +16,11 @@ export default class Lots extends React.Component {
       selectedPaymentMethods: [],
       availableDeliveryMethods: [],
       selectedDeliveryMethods: [],
+      pagesCount: 1
     };
   }
 
-  changeActivePage = (page) => {
-    this.setState({ activePage: page })
-  }
-
-  handleNextPage = () => {
-    this.setState((prevState) => ({ activePage: ++prevState.activePage }))
-  }
-
-  handlePrevPage = () => {
-    this.setState((prevState) => ({ activePage: --prevState.activePage }))
-  }
-
-  componentDidMount() {
+  componentDidMount = () => {
     const payments = [...new Set(...LotsData.map(_ => _.payment))];
     const deliveries = [...new Set(...LotsData.map(_ => _.delivery))];
     this.setState({
@@ -64,16 +29,17 @@ export default class Lots extends React.Component {
       selectedPaymentMethods: payments,
       availableDeliveryMethods: deliveries,
       selectedDeliveryMethods: deliveries,
+      pagesCount: Math.ceil(LotsData.length / 9),
     });
+  }
+
+  changeActivePage = (page) => {
+    this.setState({ activePage: page })
   }
 
   handlePaymentChange = payment => event => {
     event.preventDefault();
     const currentPaymentMethods = [...this.state.selectedPaymentMethods];
-    console.log(currentPaymentMethods)
-    console.log(payment)
-    console.log(currentPaymentMethods.includes(payment))
-    console.log(currentPaymentMethods.indexOf(payment))
     if (currentPaymentMethods.includes(payment)) {
       currentPaymentMethods.splice(currentPaymentMethods.indexOf(payment), 1)
     } else currentPaymentMethods.push(payment)
@@ -91,7 +57,6 @@ export default class Lots extends React.Component {
 
   }
 
-
   render() {
     const {
       lots,
@@ -100,11 +65,8 @@ export default class Lots extends React.Component {
       selectedPaymentMethods,
       availableDeliveryMethods,
       selectedDeliveryMethods,
+      pagesCount,
     } = this.state;
-
-    console.log('RENDER', selectedPaymentMethods)
-
-    const pagesArray = [...new Array(pagesCount)].map((_, index) => index + 1)
     return (
       <section className="lots-section">
         <div className="lots-list-content">
@@ -202,37 +164,10 @@ export default class Lots extends React.Component {
             </div>
           </section>
           <section className="lots">
-            {/*<h1>Lots:</h1>*/}
             <div className="lots-grid">
-              {lots.map(lot => <LotCard {...lot} key={lot.name} />)}
+              {lots.slice((activePage - 1) * 9, (activePage - 1) * 9 + 9).map(lot => <LotCard {...lot} key={lot.name} />)}
             </div>
-            <div className="pagination">
-              {activePage !== 1
-                ? (
-                  <div className="pagination-item" onClick={this.handlePrevPage}>
-                    <i className="fas fa-chevron-left"></i>
-                  </div>
-                )
-                : null
-              }
-
-
-              {pagesArray.map((page) => (
-                <div key={page} className={`pagination-item ${activePage === page ? 'active' : ''}`} onClick={this.changeActivePage.bind(this, page)}>
-                  {page}
-                </div>
-              ))}
-
-              {activePage !== pagesArray[pagesArray.length - 1]
-                ? (
-                  <div className="pagination-item" onClick={this.handleNextPage}>
-                    <i className="fas fa-chevron-right"></i>
-                  </div>
-                )
-                : null
-              }
-
-            </div>
+            <Pagination changeActivePage={this.changeActivePage} active={activePage} pageCount={pagesCount}/>
           </section>
         </div>
       </section>
