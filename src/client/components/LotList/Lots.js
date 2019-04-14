8d68eb19839d6d024fa1937d9e2aa32e58b72780
MishaHnatyshyn <gnatyshynmisha@gmail.com>
2019-04-14 23:38:44 +0300

    Filters

--- src/client/components/LotList/Lots.js
+++ src/client/components/LotList/Lots.js
@@ -4,20 +4,46 @@ import LotCard from './LotCard';
 import Pagination from "./Pagination";
 import LotsData from "./StaticData";
 
-// #TODO filter bugs
+const CheckBox = ({id, name, checked, handler, label, value}) => (
+  <label className="check-box-container">
+    {label}
+    <input type="checkbox"  name={name} id={id} value={value} checked={checked} onChange={handler} />
+    <span className="checkmark" />
+  </label>
+)
+
+const RadioButton = ({id, name, checked, handler, label, value}) => (
+  <label className="radio-button-container">
+    {label}
+    <input type="radio" name={name} id={id} value={value} checked={checked} onChange={handler} />
+    <span className="checkmark"/>
+  </label>
+)
 
 export default class Lots extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       lots: [],
-      activePage: 2,
+      priceFrom: 0,
+      priceTo: 0,
+      activePage: 1,
       availablePaymentMethods: [],
       selectedPaymentMethods: [],
       availableDeliveryMethods: [],
       selectedDeliveryMethods: [],
-      pagesCount: 1
+      pagesCount: 1,
+      visibleLots: [],
+      sortFunc: () => false
     };
+    this.sortFuncs = {
+      dateAZ: (a, b) => Date.parse(a.timestamp) -Date.parse(b.timestamp),
+      dateZA: (a, b) =>  Date.parse(b.timestamp) - Date.parse(a.timestamp),
+      priceAZ: (a, b) => a.startPrice - b.startPrice,
+      priceZA: (a, b) => b.startPrice - a.startPrice,
+      nameAZ: (a, b) => a.name.localeCompare(b.name),
+      nameZA: (a, b) => b.name.localeCompare(a.name),
+    }
   }
 
   componentDidMount = () => {
@@ -25,6 +51,7 @@ export default class Lots extends React.Component {
     const deliveries = [...new Set(...LotsData.map(_ => _.delivery))];
     this.setState({
       lots: LotsData,
+      visibleLots: LotsData,
       availablePaymentMethods: payments,
       selectedPaymentMethods: payments,
       availableDeliveryMethods: deliveries,
@@ -33,28 +60,65 @@ export default class Lots extends React.Component {
     });
   }
 
+  changeSortFunc = (e) => {
+    const func = e.target.value;
+    this.setState({ sortFunc: this.sortFuncs[func] }, this.updateVisibleLots)
+  }
+
+  updateVisibleLots = () => {
+    const newLots = this.state.lots.filter(this.filter);
+    newLots.sort(this.state.sortFunc)
+    this.setState({
+      activePage: 1,
+      visibleLots: newLots,
+      pagesCount: Math.ceil(newLots.length / 9)
+    })
+  }
+
+  changePriceRange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value }, this.updateVisibleLots)
+  }
+
   changeActivePage = (page) => {
     this.setState({ activePage: page })
   }
 
   handlePaymentChange = payment => event => {
-    event.preventDefault();
     const currentPaymentMethods = [...this.state.selectedPaymentMethods];
     if (currentPaymentMethods.includes(payment)) {
       currentPaymentMethods.splice(currentPaymentMethods.indexOf(payment), 1)
     } else currentPaymentMethods.push(payment)
     console.log(currentPaymentMethods)
-    this.setState({ selectedPaymentMethods: currentPaymentMethods})
+    this.setState({ selectedPaymentMethods: currentPaymentMethods}, this.updateVisibleLots)
   }
 
   handleDeliveryChange = delivery => event => {
-    event.preventDefault();
     const currentDeliveryMethods = [...this.state.selectedDeliveryMethods];
     if (currentDeliveryMethods.includes(delivery)) {
       currentDeliveryMethods.splice(currentDeliveryMethods.indexOf(delivery), 1)
     } else currentDeliveryMethods.push(delivery)
-    this.setState({ selectedDeliveryMethods: currentDeliveryMethods})
+    this.setState({ selectedDeliveryMethods: currentDeliveryMethods}, this.updateVisibleLots)
+  }
 
+  filter = (lot) => {
+    const {
+      selectedPaymentMethods,
+      selectedDeliveryMethods,
+      priceFrom,
+      priceTo
+    } = this.state;
+    if (priceFrom && lot.startPrice < priceFrom) return false;
+    if (priceTo && lot.startPrice > priceTo) return false;
+    if (
+      selectedPaymentMethods.length
+      && selectedPaymentMethods.filter((_) => lot.payment.includes(_)).length
+    ) return false;
+    if (
+      selectedDeliveryMethods.length
+      && selectedDeliveryMethods.filter((_) => lot.delivery.includes(_)).length
+    ) return false;
+    return true;
   }
 
   render() {
@@ -66,6 +130,10 @@ export default class Lots extends React.Component {
       availableDeliveryMethods,
       selectedDeliveryMethods,
       pagesCount,
+      priceFrom,
+      priceTo,
+      visibleLots
+
     } = this.state;
     return (
       <section className="lots-section">
@@ -78,33 +146,27 @@ export default class Lots extends React.Component {
                 <div className="filter-title">Price</div>
                 <div className="filter-list">
                   <div>
-                    <input type="radio" id="priceAZ" name="price-sort" />
-                    <label htmlFor="priceAZ">From lower to higher</label>
+                    <RadioButton id="priceAZ" name="sort" value="priceAZ" label="From lower to higher" handler={this.changeSortFunc}/>
                   </div>
                   <div>
-                    <input type="radio" id="priceZA" name="price-sort" />
-                    <label htmlFor="priceZA">From higher to lower</label>
+                    <RadioButton id="priceZA" name="sort" value="priceZA" label="From higher to lower" handler={this.changeSortFunc}/>
                   </div>
                   <div className="price-range">
                     <div>
-                      <input type="text" placeholder="From" id="price-from"/>-
-                      <input type="text" placeholder="To" id="price-to"/>
+                      <input type="text" placeholder="From" id="price-from" name="priceFrom" value={priceFrom || ''} onChange={this.changePriceRange}/>-
+                      <input type="text" placeholder="To" id="price-to" name="priceTo" value={priceTo || ''} onChange={this.changePriceRange}/>
                     </div>
                   </div>
-
                 </div>
               </div>
-
               <div className="filter-container">
                 <div className="filter-title">Name</div>
                 <div className="filter-list">
                   <div>
-                    <input type="radio" id="dateAZ" name="date-sort" />
-                    <label htmlFor="priceAZ">From A to Z</label>
+                    <RadioButton id="nameAZ" value="nameAZ" name="sort" label="From A to Z" handler={this.changeSortFunc}/>
                   </div>
                   <div>
-                    <input type="radio" id="dateZA" name="date-sort" />
-                    <label htmlFor="priceZA">From Z to A</label>
+                    <RadioButton id="nameZA" value="nameZA" name="sort" label="From Z to A" handler={this.changeSortFunc}/>
                   </div>
                 </div>
               </div>
@@ -113,12 +175,10 @@ export default class Lots extends React.Component {
                 <div className="filter-title">Date</div>
                 <div className="filter-list">
                   <div>
-                    <input type="radio" id="dateAZ" name="date-sort" />
-                    <label htmlFor="priceAZ">From A to Z</label>
+                    <RadioButton id="dateAZ" value="dateAZ" name="sort" label="From A to Z" handler={this.changeSortFunc}/>
                   </div>
                   <div>
-                    <input type="radio" id="dateZA" name="date-sort" />
-                    <label htmlFor="priceZA">From Z to A</label>
+                    <RadioButton id="dateZA" value="dateZA" name="sort" label="From Z to A" handler={this.changeSortFunc}/>
                   </div>
                 </div>
               </div>
@@ -129,14 +189,14 @@ export default class Lots extends React.Component {
                 <div className="filter-list">
                   {availablePaymentMethods.map(payment => (
                     <div key={payment}>
-                      <input
-                        type="checkbox"
+                      <CheckBox
                         id="payment1"
                         name="payment"
+                        value={payment}
+                        label={payment}
                         checked={selectedPaymentMethods.includes(payment)}
-                        onChange={this.handlePaymentChange(payment)}
+                        handler={this.handlePaymentChange(payment)}
                       />
-                      <label htmlFor="payment1">{payment}</label>
                     </div>
                   ))}
                 </div>
@@ -148,15 +208,15 @@ export default class Lots extends React.Component {
                 <div className="filter-list">
                   {availableDeliveryMethods.map(delivery => (
                     <div key={delivery}>
-                      <input
+                      <CheckBox
                         type="checkbox"
                         id="delivery1"
                         name="delivery"
                         value={delivery}
+                        label={delivery}
                         checked={selectedDeliveryMethods.includes(delivery)}
-                        onChange={this.handleDeliveryChange(delivery)}
+                        handler={this.handleDeliveryChange(delivery)}
                       />
-                      <label htmlFor="delivery1">{delivery}</label>
                     </div>
                   ))}
                 </div>
@@ -165,7 +225,7 @@ export default class Lots extends React.Component {
           </section>
           <section className="lots">
             <div className="lots-grid">
-              {lots.slice((activePage - 1) * 9, (activePage - 1) * 9 + 9).map(lot => <LotCard {...lot} key={lot.name} />)}
+              {visibleLots.filter(this.filter).slice((activePage - 1) * 9, (activePage - 1) * 9 + 9).map(lot => <LotCard {...lot} key={lot.name} />)}
             </div>
             <Pagination changeActivePage={this.changeActivePage} active={activePage} pageCount={pagesCount}/>
           </section>
