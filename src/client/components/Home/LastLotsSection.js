faca1fd86f770cef4337b269af01c4e2c46ec1f2
MishaHnatyshyn <gnatyshynmisha@gmail.com>
2019-05-06 22:00:37 +0300

    style refactoring

--- src/client/components/Home/LastLotsSection.js
+++ src/client/components/Home/LastLotsSection.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import LotsGrid from './LotsGrid';
-import SectionTitle from './SectionTitle'
+import SectionTitle from './SectionTitle';
 
-export default class LastLotsSection extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <section className="last-lot-section">
-        <div>
-          <SectionTitle text="Last lots" />
-          <LotsGrid />
-        </div>
-      </section>
-
-    );
-  }
+export default function LastLotsSection() {
+  return (
+    <section className="last-lot-section">
+      <div>
+        <SectionTitle text="Last lots" />
+        <LotsGrid />
+      </div>
+    </section>
+  );
 }
